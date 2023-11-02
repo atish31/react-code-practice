6def2a97b4f252f1fe29d28a9b33266c764a65a6
Atish Raina <atish.raina91@gmail.com>
2023-11-02 14:50:45 +0530

    Update react hook example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Accordian } from './Accordion/Accordion';
 import './App.css';
 import { Counter  } from './Counter/Counter';
 import { Pagination } from './Pagination/Pagination';
+import { Form } from './Forms/Form' 
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Counter />
       <Accordian />
       <Pagination />
+      < Form />
     </>
   );
 }
